d0956ce64e301104b5f7fc11b40a02fbc8f583f3
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-08-18 12:11:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def get_timeline(username):
 		ids.append(info.id)
 		at.append(info.created_at)
 		text.append(info.full_text)
-		result = pd.DataFrame(list(zip(ids,at,text)),columns =['TweetID','Create_At','Tweet_Text'])
+	result = pd.DataFrame(list(zip(ids,at,text)),columns =['TweetID','Create_At','Tweet_Text'])
 	return result
 
 def predict_emotions(docx):
@@ -87,76 +87,78 @@ emotions_emoji_dict = {"anger":"😠","disgust":"🤮", "fear":"😨😱", "happ
 # Main Application
 def main():
     st.sidebar.title("Twitter Behavior Observation")
-    usr = st.sidebar.text_input("Input Twitter User",value="")
-    if st.sidebar.button("Analyze User"):
-      st.sidebar.write(f'Twitter Username: {usr}')
-    menu = ["Tweet Analyzer","Tweet Network","Recommendation"]
+    menu = ["User Monitoring","Tweet Analyzer","Tweet Network","Recommendation"]
     choice = st.sidebar.selectbox("Select Menu", menu)
-    if choice == "Tweet Analyzer":
-        st.subheader("Tweet Analyzer")
-	df = get_timeline(usr)
-	search_text = df['Tweet'][0]
+    if choice == "User Monitoring":
+    	st.subheader("User's Behavior Monitoring") 
+    elif choice == "Tweet Analyzer":
+	usr = st.sidebar.text_input("Input Twitter User",value="")
+	if st.sidebar.button("Analyze User"):
+		st.sidebar.write(f'Twitter Username: {usr}')
+        	st.subheader("Tweet Analyzer")
+		df = get_timeline(usr)
+		search_text = df['Tweet'][0]
         #with st.form(key='emotion_clf_form'):
         #   search_text = st.text_area("Type Here")
         #   submit_text = st.form_submit_button(label='Submit')
 
-        if submit_text:	
-            hasilSearch = api.search_tweets(q=str(search_text),count=2)
-            texts = []
-            for tweet in hasilSearch:
-                texts.append(tweet.text)
-            # raw_text2 = texts[1]
-            raw_text = texts[0]
-            # translated = translator.translate(raw_text)
-            # translated = raw_text
-            prediction = predict_emotions(raw_text)
-            probability = get_prediction_proba(raw_text)
-            sentiment = predict_sentiment(raw_text)
-            proba_sentiment = get_sentiment_proba(raw_text)
-            col1,col2  = st.beta_columns(2)
-            with col1:
-                st.success("Search Result")
-                st.write(raw_text)
-                # st.write(raw_text2)
-                # st.write(translated.text)
-                st.success("Prediction")
-                emoji_icon = emotions_emoji_dict[prediction]
-                st.write("{}:{}".format(prediction,emoji_icon))
-                st.write("Confidence:{}".format(np.max(probability)))
-                st.write("Confidence:{}".format(np.max(proba_sentiment)))
-            with col2:
-                st.success("Prediction Probability")
-                # st.write(probability)
-                # st.write(proba_sentiment)
-                proba_df = pd.DataFrame(probability,columns=pipe_lr.classes_)
-                proba_sent_df = pd.DataFrame(proba_sentiment,columns=pipe_lr.classes_)
-                # st.write(proba_df.T)
-                # st.write(proba_sent_df.T)
-                # proba_df_clean = proba_df.T.reset_index()
-                # proba_df_clean.columns = ["emotions","probability"]
-                proba_df_sent_clean = proba_sent_df.T.reset_index()
-                proba_df_sent_clean.columns = ["sentiments","probability"]
+        #if submit_text:	
+		hasilSearch = api.search_tweets(q=str(search_text),count=2)
+		texts = []
+		for tweet in hasilSearch:
+		texts.append(tweet.text)
+		# raw_text2 = texts[1]
+		raw_text = texts[0]
+		# translated = translator.translate(raw_text)
+		# translated = raw_text
+		prediction = predict_emotions(raw_text)
+		probability = get_prediction_proba(raw_text)
+		sentiment = predict_sentiment(raw_text)
+		proba_sentiment = get_sentiment_proba(raw_text)
+		col1,col2  = st.beta_columns(2)
+		with col1:
+		st.success("Search Result")
+		st.write(raw_text)
+		# st.write(raw_text2)
+		# st.write(translated.text)
+		st.success("Prediction")
+		emoji_icon = emotions_emoji_dict[prediction]
+		st.write("{}:{}".format(prediction,emoji_icon))
+		st.write("Confidence:{}".format(np.max(probability)))
+		st.write("Confidence:{}".format(np.max(proba_sentiment)))
+		with col2:
+		st.success("Prediction Probability")
+		# st.write(probability)
+		# st.write(proba_sentiment)
+		proba_df = pd.DataFrame(probability,columns=pipe_lr.classes_)
+		proba_sent_df = pd.DataFrame(proba_sentiment,columns=pipe_lr.classes_)
+		# st.write(proba_df.T)
+		# st.write(proba_sent_df.T)
+		# proba_df_clean = proba_df.T.reset_index()
+		# proba_df_clean.columns = ["emotions","probability"]
+		proba_df_sent_clean = proba_sent_df.T.reset_index()
+		proba_df_sent_clean.columns = ["sentiments","probability"]
 
-                # fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions',y='probability',color='emotions')
-                # st.altair_chart(fig,use_container_width=True)
-                fig = alt.Chart(proba_df_sent_clean).mark_bar().encode(x='sentiments',y='probability',color='sentiments')
-                st.altair_chart(fig,use_container_width=True)
+		# fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions',y='probability',color='emotions')
+		# st.altair_chart(fig,use_container_width=True)
+		fig = alt.Chart(proba_df_sent_clean).mark_bar().encode(x='sentiments',y='probability',color='sentiments')
+		st.altair_chart(fig,use_container_width=True)
     elif choice == "Tweet Network":
-        data = st.file_uploader("Upload Dataset", type=["csv","txt"])
-        if data is not None:
-            df = pd.read_csv(data)
-            st.dataframe(df.head())
-        else:
-            st.write("No Dataset To Show")
-        st.subheader("Exploratory Data Analysis")
-        if data is not None:
-            if st.checkbox("Show Shape"):
-                st.write(df.shape)
-            if st.checkbox("Show Summary"):
-                st.write(df.describe())
-            if st.checkbox("Correlation Matrix"):
-                st.write(sns.heatmap(df.corr(),annot=True))
-                st.pyplot()
+	data = st.file_uploader("Upload Dataset", type=["csv","txt"])
+	if data is not None:
+	    df = pd.read_csv(data)
+	    st.dataframe(df.head())
+	else:
+	    st.write("No Dataset To Show")
+	st.subheader("Exploratory Data Analysis")
+	if data is not None:
+	    if st.checkbox("Show Shape"):
+		st.write(df.shape)
+	    if st.checkbox("Show Summary"):
+		st.write(df.describe())
+	    if st.checkbox("Correlation Matrix"):
+		st.write(sns.heatmap(df.corr(),annot=True))
+		st.pyplot()
     elif choice == "Recommendation":
         st.write("Channel to follow")
 
