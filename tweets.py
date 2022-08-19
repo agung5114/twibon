b930eb266a845b66d93d887679525bc576bf3c3a
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-08-19 23:54:24 +0700

    Update tweets.py

--- tweets.py
+++ tweets.py
@@ -5,10 +5,6 @@ api_key = st.secrets["api_key"]
 api_secret_key = st.secrets["api_secret_key"]
 access_token = st.secrets["access_token"]
 access_token_secret = st.secrets["access_token_secret"]
-# api_key = "l5FGlSMhD3FOB1phnwB7I9sX5"
-# api_secret_key = "R55gay8XG4uz1VGns8BT87zzXBGftNxPMaS9nvUVOzRI8YNsP1"
-# access_token = "237213820-RbW5PBW76TqcbT1tiAGjdkiMV7LlPnRIb9oDHixg"
-# access_token_secret = "EkHdik9UpmPB8CP8g3kSip0RC30LqgSRdkuGrovUnNEyN"
 auth = tweepy.OAuthHandler(api_key,api_secret_key)
 auth.set_access_token(access_token,access_token_secret)
 api = tweepy.API(auth)
